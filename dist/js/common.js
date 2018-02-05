d976616f117da873172ba3f79024145abe46a0b3
Mynik3d <sanamah550@gmail.com>
2018-02-05 16:33:59 +0300

    first commit

---
+++ dist/js/common.js
@@ -0,0 +1,12 @@
+$('.bg-home__slider').slick({
+	 infinite: true,
+	 // autoplay: true,
+	 speed: 300,
+	 adaptiveHeight: true,
+	
+
+	  prevArrow: '<div class="prev"></div>',
+nextArrow: '<div class="next"></div>'
+});
+
+
